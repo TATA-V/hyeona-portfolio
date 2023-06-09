9cbed1dfa219254254d8ee5fdbef8f26fa06b3ea
Choi-hyeona <xhrl523@naver.com>
2023-06-09 20:21:54 +0900

    헤더에 마우스 커서 추가

--- src/components/Head/Head.tsx
+++ src/components/Head/Head.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useState } from "react";
 
 import RightButton from "./HeadMaterial/RightButton";
 
@@ -40,6 +41,20 @@ const RealHead = styled.div`
   }
 `;
 
+const Cursor = styled.div<ICursor>`
+  position: fixed;
+  width: ${({ isLogoHovered }) => (isLogoHovered ? "45px" : "20px")};
+  height: ${({ isLogoHovered }) => (isLogoHovered ? "45px" : "20px")};
+  border-radius: 50%;
+  background-color: rgba(246, 219, 244, 0.22);
+  pointer-events: none;
+  z-index: 9999;
+  transition: transform 0.2s ease;
+  top: ${({ cursorPosition, isLogoHovered }) => (isLogoHovered ? `${cursorPosition.y - 30}px` : `${cursorPosition.y - 10}px`)};
+  left: ${({ cursorPosition, isLogoHovered }) => (isLogoHovered ? `${cursorPosition.x - 30}px` : `${cursorPosition.x - 10}px`)};
+  transform: ${({ isLogoHovered }) => (isLogoHovered ? "scale(1.5)" : "none")};
+`;
+
 interface IButtonTxt {
   id: number;
   text: string;
@@ -65,7 +80,55 @@ const buttonTxt: IButtonTxt[] = [
   },
 ];
 
+interface ICursor {
+  cursorPosition: { x: number; y: number };
+  isLogoHovered: boolean;
+}
+
 const Head = () => {
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+  const [isCursorVisible, setCursorVisible] = useState(true);
+  const [isLogoHovered, setLogoHovered] = useState(false);
+  const [isButtonHovered, setButtonHovered] = useState(false);
+
+  // 마우스 이동 이벤트 핸들러
+  const handleMouseMove: React.MouseEventHandler<HTMLElement> = (e) => {
+    const { clientX, clientY } = e;
+    setCursorPosition({ x: clientX, y: clientY });
+
+    // 헤더 영역 내에 있는지 확인
+    const headerRect = e.currentTarget.getBoundingClientRect();
+    const isInsideHeader = clientX >= headerRect.left && clientX <= headerRect.right && clientY >= headerRect.top && clientY <= headerRect.bottom;
+
+    setCursorVisible(isInsideHeader);
+  };
+
+  const handleMouseLeave = () => {
+    setCursorVisible(false);
+  };
+
+  // 로고
+  // 마우스 올라갔을 때
+  const handleLogoMouseEnter = () => {
+    setLogoHovered(true);
+  };
+
+  // 마우스 벗어났을 때
+  const handleLogoMouseLeave = () => {
+    setLogoHovered(false);
+  };
+
+  // 오른쪽 버튼
+  // 마우스 올라갔을 때
+  const handleButtonMouseEnter = () => {
+    setButtonHovered(true);
+  };
+
+  // 마우스 벗어났을 때
+  const handleButtonMouseLeave = () => {
+    setButtonHovered(false);
+  };
+
   const handleLogoClick = () => {
     window.scrollTo({
       top: 0,
@@ -74,14 +137,15 @@ const Head = () => {
   };
 
   return (
-    <HeadBlock>
+    <HeadBlock onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave}>
       <RealHead>
-        <button onClick={handleLogoClick} className="logo">
+        {isCursorVisible && !isButtonHovered && <Cursor cursorPosition={cursorPosition} isLogoHovered={isLogoHovered} />}
+        <button onClick={handleLogoClick} className="logo" onMouseEnter={handleLogoMouseEnter} onMouseLeave={handleLogoMouseLeave}>
           HYEONA'S PORTFOLIO
         </button>
 
         {/* 오른쪽 버튼들 => RightButton 컴포넌트 */}
-        <div className="right-btn-box">
+        <div className="right-btn-box" onMouseEnter={handleButtonMouseEnter} onMouseLeave={handleButtonMouseLeave}>
           {buttonTxt.map((el) => (
             <RightButton key={el.id} text={el.text} />
           ))}
