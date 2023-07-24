edd02b6ca552a6b651496d1e8a6e9f79ba90dc4b
Choi-hyeona <xhrl523@naver.com>
2023-07-24 20:53:22 +0900

    skills 이미지 수정

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,11 +1,8 @@
-import styled from "styled-components";
-import starBar from "../../assets/png-file/star-bar.png";
-import languages from "../../assets/png-file/languages.png";
-import languagesTxt from "../../assets/png-file/languagesTxt.png";
-import frameLibraries from "../../assets/png-file/frame-libraries.png";
-import frameLibrariesTxt from "../../assets/png-file/frameLibrariesTxt.png";
-import others from "../../assets/png-file/others.png";
-import othersTxt from "../../assets/png-file/othersTxt.png";
+import styled from 'styled-components';
+import starBar from '../../assets/png-file/star-bar.png';
+import languages from '../../assets/png-file/languages.png';
+import frameLibraries from '../../assets/png-file/frame-libraries.png';
+import others from '../../assets/png-file/others.png';
 
 const SkillsBlock = styled.div`
   width: 100%;
@@ -119,19 +116,6 @@ const Languages = styled.div`
     background: no-repeat url(${languages});
     background-size: 579.5px 180px;
     display: flex;
-
-    .js {
-      padding: 9px 49.5px 0 30px;
-    }
-    .ts {
-      padding: 9px 47.5px 0 17px;
-    }
-    .html {
-      padding: 9px 64.5px 0 34px;
-    }
-    .css {
-      padding: 9px 72.5px 0 43px;
-    }
   }
 
   @media all and (min-width: 768px) and (max-width: 1023px) {
@@ -148,23 +132,6 @@ const Languages = styled.div`
       background: no-repeat url(${languages});
       background-size: cover;
       display: flex;
-
-      .js {
-        padding: 7px 0 0 19px;
-        font-size: 15px;
-      }
-      .ts {
-        padding: 7px 0 0 47px;
-        font-size: 15px;
-      }
-      .html {
-        padding: 7px 0 0 60px;
-        font-size: 15px;
-      }
-      .css {
-        padding: 7px 0 0 77px;
-        font-size: 15px;
-      }
     }
   }
 
@@ -176,28 +143,21 @@ const Languages = styled.div`
     .js-ts-html-css {
       width: 275px;
       height: 85px;
-      background: no-repeat url(${languagesTxt});
+      background: no-repeat url(${languages});
       background-size: cover;
       display: flex;
-
-      .js,
-      .ts,
-      .html,
-      .css {
-        display: none;
-      }
     }
   }
 `;
 
-const FrameAndLibraries = styled.div`
+const Others = styled.div`
   width: 427px;
   height: 221px;
 
   transform: translate(545px, -77px);
 
   .lilac-line {
-    width: 217px;
+    width: 64px;
     height: 2px;
     background-color: #f6f1f8;
     margin: 5px 0 13px auto;
@@ -210,23 +170,12 @@ const FrameAndLibraries = styled.div`
   .react-redux-styled {
     width: 426.09px;
     height: 176px;
-    background: no-repeat url(${frameLibraries});
+    background: no-repeat url(${others});
     background-size: 426.09px 176px;
   }
 
   .react-redux-styled {
     display: flex;
-
-    .react {
-      padding: 9px 0 0 65px;
-    }
-    .redux {
-      padding: 9px 0 0 95px;
-    }
-    .styled {
-      text-align: center;
-      padding: 9px 0 0 36px;
-    }
   }
 
   @media all and (min-width: 768px) and (max-width: 1023px) {
@@ -235,28 +184,15 @@ const FrameAndLibraries = styled.div`
     width: 329px;
 
     .lilac-line {
-      width: 170px;
+      width: 52px;
     }
 
     .react-redux-styled {
       width: 329.64px;
       height: 136px;
-      background: no-repeat url(${frameLibraries});
+      background: no-repeat url(${others});
       background-size: cover;
       display: flex;
-
-      .react {
-        padding: 8px 0 0 47px;
-        font-size: 15px;
-      }
-      .redux {
-        padding: 8px 0 0 71px;
-        font-size: 15px;
-      }
-      .styled {
-        padding: 8px 0 0 25px;
-        font-size: 14px;
-      }
     }
   }
 
@@ -266,34 +202,28 @@ const FrameAndLibraries = styled.div`
     height: 221px;
 
     .lilac-line {
-      width: 150px;
+      width: 46px;
     }
 
     .react-redux-styled {
       transform: translate(118px, 0);
       width: 206px;
       height: 85px;
-      background: no-repeat url(${frameLibrariesTxt});
+      background: no-repeat url(${others});
       background-size: cover;
       display: flex;
-
-      .react,
-      .redux,
-      .styled {
-        display: none;
-      }
     }
   }
 `;
 
-const Others = styled.div`
+const FrameAndLibraries = styled.div`
   width: 581.5px;
   height: 225px;
 
   transform: translate(0, -155px);
 
   .lilac-line {
-    width: 63px;
+    width: 221px;
     height: 2px;
     background-color: #f6f1f8;
     margin: 5px 0 13px 0;
@@ -302,76 +232,39 @@ const Others = styled.div`
   .git-s3-postman-figma {
     width: 579.5px;
     height: 180px;
-    background: no-repeat url(${others});
+    background: no-repeat url(${frameLibraries});
     background-size: cover;
     display: flex;
-
-    .git {
-      padding: 9px 74.5px 0 56px;
-    }
-    .s3 {
-      padding: 9px 54.5px 0 24px;
-    }
-    .postman {
-      padding: 9px 53.5px 0 23px;
-    }
-    .figma {
-      padding: 9px 63.5px 0 34px;
-    }
   }
 
   @media all and (min-width: 768px) and (max-width: 1023px) {
     width: 600px;
 
     .lilac-line {
-      width: 52px;
+      width: 170px;
     }
 
     .git-s3-postman-figma {
       width: 440px;
       height: 136px;
-      background: no-repeat url(${others});
+      background: no-repeat url(${frameLibraries});
       background-size: cover;
       display: flex;
-
-      .git {
-        padding: 8px 0 0 42px;
-        font-size: 15px;
-      }
-      .s3 {
-        padding: 8px 0 0 72px;
-        font-size: 15px;
-      }
-      .postman {
-        padding: 8px 0 0 54px;
-        font-size: 15px;
-      }
-      .figma {
-        padding: 8px 0 0 62px;
-        font-size: 15px;
-      }
     }
   }
 
   @media all and (max-width: 767px) {
     .lilac-line {
-      width: 46px;
+      width: 152px;
     }
 
     .git-s3-postman-figma {
       width: 275px;
       height: 85px;
-      background: no-repeat url(${othersTxt});
+      background: no-repeat url(${frameLibraries});
       background-size: cover;
       display: flex;
       justify-content: space-between;
-
-      .git,
-      .s3,
-      .postman,
-      .figma {
-        display: none;
-      }
     }
   }
 `;
@@ -388,34 +281,20 @@ const Skills = () => {
         <Languages>
           <p className="skills-bold">Programming Languages</p>
           <div className="lilac-line"></div>
-          <div className="js-ts-html-css">
-            <p className="js">Javascript</p>
-            <p className="ts">Typescript</p>
-            <p className="html">HTML</p>
-            <p className="css">CSS</p>
-          </div>
+          <div className="js-ts-html-css"></div>
         </Languages>
 
-        <FrameAndLibraries>
-          <p className="skills-bold frame">Frameworks & Libraries</p>
-          <div className="lilac-line"></div>
-          <div className="react-redux-styled">
-            <p className="react">React</p>
-            <p className="redux">Redux</p>
-            <p className="styled">Styled- Components</p>
-          </div>
-        </FrameAndLibraries>
-
         <Others>
-          <p className="skills-bold">Others</p>
+          <p className="skills-bold frame">Others</p>
           <div className="lilac-line"></div>
-          <div className="git-s3-postman-figma">
-            <p className="git">Git</p>
-            <p className="s3">AWS(S3)</p>
-            <p className="postman">Postman</p>
-            <p className="figma">Figma</p>
-          </div>
+          <div className="react-redux-styled"></div>
         </Others>
+
+        <FrameAndLibraries>
+          <p className="skills-bold">Frameworks & Libraries</p>
+          <div className="lilac-line"></div>
+          <div className="git-s3-postman-figma"></div>
+        </FrameAndLibraries>
       </RealSkillsBlock>
     </SkillsBlock>
   );
